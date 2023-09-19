f239c98a5647266e68372b0f56585d0aa0780a4e
yuvasri1907 <144417825+yuvasri1907@users.noreply.github.com>
2023-09-19 08:51:31 +0530

    Update challenge 2.py

--- challenge 2.py
+++ challenge 2.py
@@ -1,14 +1,9 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-# Input from the user
-num = int(input("Enter a non-negative integer: "))
-
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    result = factorial(num)
-    print(f"The factorial of {num} is {result}")
+def CheckLeap(Year):  
+  if((Year % 400 == 0) or  
+     (Year % 100 != 0) and  
+     (Year % 4 == 0)):   
+    print("Given Year is a leap Year"); 
+  else:  
+    print ("Given Year is not a leap Year")  
+Year = int(input("Enter the number: "))
+CheckLeap(Year)
